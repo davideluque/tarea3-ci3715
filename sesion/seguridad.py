43a56b669635e7459bd630319ef3578ffcb4cee8
David Cabeza <cabezadavide@gmail.com>
2018-05-18 00:53:20 -0400

    Añade nuevos test para validación de contraseña

--- sesion/seguridad.py
+++ sesion/seguridad.py
@@ -4,6 +4,7 @@ class Seguridad:
 
     def __init__(self):
         self.usuariosRegistrados = {}
+        self.error_msg = ""
 
     def correoValido(self, eMail):
         # Verificamos si el email cumple el formato RFC 822.
@@ -22,3 +23,40 @@ class Seguridad:
         if(claveUno != claveDos):
             return False
         return True
+
+    def validacionClave(self, clave):
+        # Verificamos la longitud de las claves.
+        if(len(clave) < 8 or len(clave) > 16):
+            self.error_msg = "Clave invalida, las claves deben tener entre 8 y 16 caracteres"
+            return False			
+
+        # Verificamos que no existan caracteres especiales en las claves.
+        if not (clave.isalnum()):
+            self.error_msg = "Clave invalida, las claves deben ser alfanumericas"
+            return False
+
+        # Verificamos que la clave tenga mas de dos letras, con al menos una mayuscula
+        # y una minuscula.
+        cantLetras = 0
+        cantMay = 0
+        for letra in clave:
+            if (letra.isalpha()):
+                cantLetras += 1
+            if(letra.isupper()):
+                cantMay += 1
+        
+        if not (cantLetras > 2 and cantMay > 0 and cantLetras - cantMay > 0):
+            self.error_msg = "Clave invalida, la clave debe tener al menos tres letras, con al menos una mayuscula y una minuscula"
+            return False
+
+        # Verificamos que la clave contenga al menos un digito
+        hayNum = False
+        for letra in clave:
+            if letra.isdigit():
+                hayNum = True
+
+        if not (hayNum):
+            self.error_msg = "Clave invalida, la clave debe contener al menos un digito"
+            return False
+
+        return True        
--- sesion/tests.py
+++ sesion/tests.py
@@ -28,3 +28,25 @@ class TestSeguridad(unittest.TestCase):
 
         for correo_invalido in correos_invalidos:
             self.assertEqual(seguridad.correoValido(correo_invalido), False)
+
+    def testContrasenaInvalida(self):
+        contrasenas_invalidas = ['1111111', # 7 Digitos
+                                 '16161616161616161', # 17 Digitos
+                                 'abu2y2353583782371', # 18 Digitos
+                                 '9235580990k', # Falta mayuscula
+                                 '9389235398K', # Falta minuscula
+                                 '%zYzaaa1111'] # Tiene caracter especial
+                                 
+        for contrasena_invalida in contrasenas_invalidas:
+            self.assertEqual(seguridad.validacionClave(contrasena_invalida), False)
+            
+    
+    def testContrasenaValida(self):
+        contrasenas_validas = ['MiClave123',
+                               'contraseNa1',
+                               'kk88Ymmm1',
+                               'Password123',
+                               'hjhadj7G']
+        
+        for contrasena_valida in contrasenas_validas:
+            self.assertEqual(seguridad.validacionClave(contrasena_valida), True)
